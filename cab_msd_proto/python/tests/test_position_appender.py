b53226c217b5072c96dffabb46dd412fb7ae86da
lmichel <laurent.michel@astro.unistra.fr>
2020-04-16 17:59:40 +0200

    test programs

--- cab_msd_proto/python/tests/test_position_appender.py
+++ cab_msd_proto/python/tests/test_position_appender.py
@@ -5,10 +5,21 @@ Created on 15 avr. 2020
 '''
 import os
 from mapper.position_appender import PositionAppender  
+from mapper.votable_merger import VOTableMerger
         
 if __name__ == '__main__':
         base_path = os.path.dirname(os.path.realpath(__file__)) 
 
+        raw_votable_path = os.path.join(
+            base_path, 
+            "../../", 
+            "data/raw_data", 
+            "xmm_detections.xml")  
+        annot_votable_path = os.path.join(
+            base_path, 
+            "../../", 
+            "data/annotated_data", 
+            "xmm_detections.annot.xml")
         cab_msd_path = os.path.join(
             base_path, 
             "../../", 
@@ -19,8 +30,23 @@ if __name__ == '__main__':
             "../../", 
             "data/mapping_components", 
             "position.mapping.xml")
+        output_mapping_path = os.path.join(
+            base_path, 
+            "../../", 
+            "data/annotated_data", 
+            "xmm_detections.mapping.xml")
         position_appender = PositionAppender(cab_msd_path, position_path)
-        position_appender.set_refs("_s_ra", "_s_ra", "_radec_err", "_iauname")
-        position_appender.print()
+        position_appender.set_identifier("namesaada")
+        position_appender.set_position("_ra_146", "_dec_147", 
+                                       "_poserr_148", "_syserrcc_152",
+                                       "deg", "arcsec")
+        position_appender.set_spaceframe("ICRS", None)
+        position_appender.set_notset_value()
+
+        print(position_appender.tostring())
+        position_appender.save(output_mapping_path)
         
- 
+        merger = VOTableMerger(raw_votable_path, output_mapping_path, annot_votable_path)
+        merger.insert_mapping()
+        
+        
--- xml_schema/python/tests/test_xmm_instancier_from_votable.py
+++ xml_schema/python/tests/test_xmm_instancier_from_votable.py
@@ -11,17 +11,33 @@ from launcher.instance_from_votable import InstanceFromVotable
 if __name__ == '__main__':
     base_path = os.path.dirname(os.path.realpath(__file__)) 
     votable_path = os.path.join(base_path, 
-                                "../../mapping_sample",
-                                "sample_ga.vot"
+                                "../../../cab_msd_proto/data/annotated_data",
+                                "xmm_detections.annot.xml"
                                 )
     instance_from_votable = InstanceFromVotable(votable_path)
     instance = instance_from_votable.build_instance()
+    
+    print(instance.get_flatten_data_head())
+    print(instance.get_data_subset_keys())
+
     while True:
-        inst = instance._get_next_row_instance()
+        inst = instance._get_next_flatten_row()
         if inst != None:
+            print(inst)
             print(json.dumps(inst, 
                              indent=2,
                              sort_keys=True,
                              cls=MyEncoder))
+            break
         else:
             break
+
+
+
+    
+    
+
+
+    
+    
+    
